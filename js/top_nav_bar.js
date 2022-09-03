2c540f82e4333aefb5e8482e678620b357c5f6f5
chris <christopher.logston@sfdpw.org>
2022-09-03 12:40:38 -0700

    modified:   data/2977J_010_PRJCT_DWGS_18.js 	modified:   data/base_sov.js 	new file:   funding/funding_PP07.html 	modified:   js/top_nav_bar.js 	new file:   pp_as_builts/2977J_CR-09_AB_PP07.pdf 	new file:   pp_as_builts/2977J_CR-10_AB_PP07.pdf 	new file:   pp_as_builts/2977J_CR-11_AB_PP07.pdf 	new file:   pp_as_builts/2977J_R-04_AB_PP07_a.pdf 	new file:   pp_as_builts/2977J_R-04_AB_PP07_b.pdf 	new file:   pp_as_builts/2977J_R-05_AB_PP07_a.pdf 	new file:   pp_as_builts/2977J_R-05_AB_PP07_b.pdf 	new file:   pp_as_builts/2977J_R-06_AB_PP07_a.pdf 	new file:   pp_as_builts/2977J_R-06_AB_PP07_b.pdf 	modified:   pp_as_builts/ab_list.js 	new file:   sov/sov_PP07.html

--- js/top_nav_bar.js
+++ js/top_nav_bar.js
@@ -54,6 +54,7 @@ function top_nav(nav_element, pp_no = null, bid_item_id = null)
 		     <a class="dropdown-item" href="' + nav_path + 'sov/sov_PP04.html">PP04 - Apr 2022</a>\
 		     <a class="dropdown-item" href="' + nav_path + 'sov/sov_PP05.html">PP05 - May 2022</a>\
 		     <a class="dropdown-item" href="' + nav_path + 'sov/sov_PP06.html">PP06 - Jun 2022</a>\
+		     <a class="dropdown-item" href="' + nav_path + 'sov/sov_PP07.html">PP07 - Jul-Aug 2022</a>\
 	           </li>\
 	         </ul>\
 	       </li>\
@@ -67,6 +68,7 @@ function top_nav(nav_element, pp_no = null, bid_item_id = null)
 	             <a class="dropdown-item" href="' + nav_path + 'funding/funding_PP04.html">PP04 - Apr 2022</a>\
 	             <a class="dropdown-item" href="' + nav_path + 'funding/funding_PP05.html">PP05 - May 2022</a>\
 	             <a class="dropdown-item" href="' + nav_path + 'funding/funding_PP06.html">PP06 - Jun 2022</a>\
+	             <a class="dropdown-item" href="' + nav_path + 'funding/funding_PP07.html">PP07 - Jul-Aug 2022</a>\
 	           </li>\
 	         </ul>\
 	       </li>\
